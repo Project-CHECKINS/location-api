83adde6e81389f5c8cab7932b19a4d572877ee69
CharlesA2K <c.bercasio@a2kgroup.org>
2025-08-08 15:08:14 +0800

    dev: spread the passed param in logger for better output

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,11 +3,11 @@ class Logger {
     return new Date().toISOString();
   }
   info(message: string, ...params: unknown[]) {
-    console.info(`[INFO] ${this.time()}: ${message}`, params);
+    console.info(`[INFO] ${this.time()} - ${message}`, ...params);
   }
 
   error(message: string, ...params: unknown[]) {
-    console.error(`[ERROR] ${this.time()}: ${message}`, params);
+    console.error(`[ERROR] ${this.time()} - ${message}`, ...params);
   }
 }
 
